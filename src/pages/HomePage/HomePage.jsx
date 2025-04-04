5cdeb962acd6893ac7fd241528311d41f4533b17
emazupp <zuppardo.emanuele2001@gmail.com>
2025-04-04 12:54:39 +0200

    💄 style: add titles and some css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,11 +8,34 @@ export default function HomePage() {
       <MenuSidebar />
       <div className={styles.jumboWrapper}>
         <div className={styles.jumboContent}>
-          <div className={styles.sectionTitle}>
-            <h1>Il tuo viaggio inizia qui</h1>
+          <div className={styles.upperSectionWrapper}>
+            <div className={styles.upperSection}>
+              <div className={styles.upperLeftSection}>
+                <span>SICILIA, ITALIA</span>
+                <div className={styles.separatorLine}></div>
+                <div className={styles.upperSectionTitles}>
+                  <span>Dalla città ai circuiti</span>
+                  <span>il lusso a noleggio per ogni occasione.</span>
+                </div>
+              </div>
+              <div className={styles.upperRightSection}>
+                <span>
+                  Luxrent offre il noleggia auto di lusso e supersportive,
+                </span>
+                <span>
+                  offrendo esperienze uniche su strada con un servizio
+                </span>
+                <span>clienti impeccabile, attento e dedicato.</span>
+              </div>
+            </div>
+            <div className={styles.sloganSection}>
+              <span>Noleggia il lusso</span>
+              <span>Vivi l'emozione.</span>
+            </div>
           </div>
           <div className={styles.jumboLowerButton}>
-            <span>Affita ora un'auto</span>
+            <span>Affitta ora</span>
+            <i class="fa-solid fa-chevron-down"></i>
           </div>
         </div>
       </div>
